614dfc90a8fa02a3d0d1ff47f089e6c1dfbff850
johnwill14 <johnwill.v.2017@gmail.com>
2025-05-12 23:18:54 -0300

    kadane

---
+++ code_example/src/main/kotlin/chapters/c003/MaxSubarraySum.kt
@@ -0,0 +1,29 @@
+package br.com.william.chapters.c003
+
+import kotlin.math.abs
+
+fun maxSubarraySum(array: IntArray): Int {
+  if (array.isEmpty()) {
+        return 0
+  }
+
+  var sum = 0
+  var ans = Int.MIN_VALUE
+  val size = array.size - 1
+
+  for(i in 0..size){
+      if (sum < 0) {
+          sum = 0
+      }
+
+      sum += array[i]
+      ans = maxOf(ans, sum)
+  }
+
+  return ans
+}
+
+fun main() {
+    println(maxSubarraySum(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)))
+    println(maxSubarraySum(intArrayOf(-2, -1, -3, -4, -1, -2, -1, -5, -4)))
+}
